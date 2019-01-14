abae275508c0808f6db4466b8fdfb666b9e9404e
shaniajain <shaniajain04@gmail.com>
2019-01-13 18:54:19 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,106 +1,114 @@
+// Shania Jain, Aneri Parikh, Lahari Vuppaladhadiam
+// 1/22/17
+// C++ Final Project
+// Snakes and Ladders - two-player game that guides players through a board with snakes (bring you down)  and ladders (carry you up) that have been pre-set. The person who reaches position 100 first wins
 
-
-/*
- Farming is competitive business -- particularly milk production. Farmer John figures that if he doesn't innovate in his milk production methods, his dairy business could get creamed!
- Fortunately, Farmer John has a good idea. His three prize dairy cows Bessie, Elsie, and Mildred each produce milk with a slightly different taste, and he plans to mix these together to get the perfect blend of flavors.
- 
- To mix the three different milks, he takes three buckets containing milk from the three cows. The buckets may have different sizes, and may not be completely full. He then pours bucket 1 into bucket 2, then bucket 2 into bucket 3, then bucket 3 into bucket 1, then bucket 1 into bucket 2, and so on in a cyclic fashion, for a total of 100 pour operations (so the 100th pour would be from bucket 1 into bucket 2). When Farmer John pours from bucket aa into bucket bb, he pours as much milk as possible until either bucket aa becomes empty or bucket bb becomes full.
- 
- Please tell Farmer John how much milk will be in each bucket after he finishes all 100 pours.
- */
 
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
-int main() {
+// checks if player’s position is at a snake
+int Snakes(int &position, int roll_dice)
+{
+    int start_snake[10] = {16, 46, 49, 62, 64, 74, 89, 92, 95, 99}; // defines the starting point of each snake
+    int end_snake[10] = {6, 25, 11, 19, 60, 53, 68, 88, 75, 80}; // defines the corresponding end point that is lower
     
-    ifstream in_file;
-    in_file.open("mixmilk.in");
-    if (in_file.fail())
+    for (int i = 0; i < 10; i++)
     {
-        cout << "error";
+        if (position == start_snake[i])
+        {
+            position = end_snake[i]; // shifts player’s position to bottom tail of snake
+            cout << "Oops! You ran into a snake! ";
+        }
+        
+    }
+    return 0;
+    
+}
+
+
+
+//checks if player’s position is at a ladder
+int ladders(int &position, int roll_dice)
+{
+    const int ARR_SIZE = 11;
+    int start_ladder[] = {2, 7, 8, 15, 21, 28, 36, 51, 71, 78, 87}; // defines the starting point of each ladder
+    int end_ladder[] = {38, 14, 31, 26, 42, 84, 44, 67, 91, 98, 94}; // defines end point of ladder
+    
+    for (int i = 0; i <= ARR_SIZE; i++)
+    {
+        if (position == start_ladder[i])
+        {
+            position = end_ladder[i]; // shifts player’s position to the end of the ladder
+            cout << "Yay! You found a ladder! ";
+        }
+    }
+    cout << "You are now at position " << position << endl << endl;
+    return 0;
+}
+
+
+
+
+int main ()
+{
+    //set initial position to 0
+    int position1 = 0;
+    int position2 = 0;
+    
+    //loop continues until either players’ position exceeds 100
+    while (position1 < 100 && position2 < 100)
+    {
+        cout << "It is player1's turn. Press '1' to play. ";
+        int input1;
+        cin >> input1;
+        
+        if (input1 == 1)
+        {
+            //generates a random number between 1 and 6
+            int dice = 1 + rand() % 6;
+            cout << "You rolled a " << dice << " ";
+            //adds number on dice to current position to get player 1’s new position
+            position1 = position1 + dice;
+            //call to Snake function
+            Snakes(position1, dice);
+            //call to ladder function
+            ladders(position1, dice);
+        }
+        
+        cout << "It is player2's turn. Press '1' to play. ";
+        int input2;
+        cin >> input2;
+        
+        if (input2 == 1)
+        {
+            //generates a random number between 1 and 6
+            int dice = rand() % 6 + 1;
+            cout << "You rolled a " << dice << " ";
+            
+            //adds number on dice to current position to get player 2’s new position
+            position2 = position2 + dice;
+            
+            // call to snake function
+            Snakes(position2, dice);
+            // call to ladder function
+            ladders(position2, dice);
+        }
     }
     
     
-    int c1;
-    in_file >> c1;
-    
-    int m1;
-    in_file >> m1;
-    
-    
-    int c2;
-    in_file >> c2;
-    
-    int m2;
-    in_file >> m2;
-    
-    int c3;
-    in_file >> c3;
-    
-    int m3;
-    in_file >> m3;
-    
-    
-    
-    for (int i = 1; i <= 33; i++) {
-        //Bucket 1 to Bucket 2
-        if ((m1 + m2) <= c2) {
-            m2 = m1 + m2;
-            m1 = 0;
-        }else if ((m1 + m2) > c2){
-            m1 = (m1 + m2) - c2;
-            m2 = c2;
-        }
-        //Bucket 2 to Bucket 3
-        if ((m2 + m3) <= c3) {
-            m3 = m2 + m3;
-            m2 = 0;
-        }else if ((m2 + m3) > c3){
-            m2 = (m2 + m3) - c3;
-            m3 = c3;
-        }
-        
-        //Bucket 3 to Bucket 1
-        
-        if ((m3 + m1) <= c1) {
-            m1 = m3 + m1;
-            m3 = 0;
-        }else if ((m3 + m1) > c1) {
-            m3 = (m1 + m3) - c1;
-            m1 = c1;
-        }
-        
-        
+    //prints the winner of the game if one of the players passes 100
+    if (position1 > 100 && position1 > position2)
+    {
+        cout << "Congrats Player1. You won!" << endl;
     }
-    
-    //Bucket 1 to Bucket 2
-    if ((m1 + m2) <= c2) {
-        m2 = m1 + m2;
-        m1 = 0;
-    }else if ((m1 + m2) > c2){
-        m1 = (m1 + m2) - c2;
-        m2 = c2;
+    else if (position2 > 100 && position2 > position1)
+    {
+        cout << "Congrats Player2. You won!" << endl;
     }
-    
-    
-    cout << m1 << endl;;
-    cout << m2 << endl;;
-    cout << m3 << endl;
-    
-    
-    ofstream out_file;
-    out_file.open("mixmilk.out");
-    out_file << m1;
-    out_file << m2;
-    out_file << m3;
-    
-    
-    
-    
-    
-    
-    
+    return 0;
 }
